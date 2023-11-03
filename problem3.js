a1506dbc8046e45658aa18b50755c03e53ff88a2
Nurs <nurrs.serkul@gmail.com>
2023-11-03 09:55:26 +0600

    all problems are solved

--- problem3.js
+++ problem3.js
@@ -2,3 +2,41 @@
 // Example string : 'webmaster'
 // Expected Output : 'abeemrstw'
 // Assume punctuation and numbers symbols are not included in the passed string.
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+function toAlphabetical(word) {
+  let result;
+  const arr = word.split("");
+  result = arr.sort().join("");
+  return result;
+}
+
+const word = "webmaster";
+console.log(toAlphabetical(word));
--- problem5.js
+++ problem5.js
@@ -1,3 +1,10 @@
 // Write a JavaScript function that takes an array of numbers and finds the second lowest and second greatest numbers, respectively.
 // Sample array : [1,2,3,4,5]
 // Expected Output : 2,4
+function lowAndGreat(numbers) {
+  const arr = numbers.sort();
+  return `${arr[1]} ${arr[arr.length - 2]}`;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(lowAndGreat(numbers));
